5289343177b0ad7ac4f3ad17eb6af3b5c5d8ab2d
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2024-11-09 09:57:06 -0300

    Update

--- ArrayControl.java
+++ ArrayControl.java
@@ -6,18 +6,7 @@ public class ArrayControl
     private static final Scanner scan = new Scanner(System.in);
     /*public static void main(String[] args) //main method, for tests
     {
-        Object[] arr = new Object[1];
-        //System.out.println("before: " + arr.length);
 
-        //arr = addArrSlotsWCopy(arr, 1);
-        //arr = toIntArr(arr);
-        //System.out.println("after: " + arr.length);
-        System.out.println("'arr' type: " + arr.getClass());
-        arr = changeArrType(arr, "string");
-        //arr[0] = 1;
-        System.out.println("value at [0]: " + arr[0] + "\n" + "'arr' type: " + arr.getClass());
-        arr = changeArrType(arr, scan.nextLine());
-        System.out.println("'arr' type: " + arr.getClass());
     }*/
 
     public static Object[] manualAddArrSlot(Object[] mainArr)
@@ -42,7 +31,7 @@ public class ArrayControl
     {
         return target = new Integer[target.length];
     }*/
-    public static Object[] changeArrType(Object[] target, String type) //NEW IMPORTANT
+    /*public static Object[] changeArrType(Object[] target, String type) //Target for conversion must be Object array
     {
         return switch (type)
         {
@@ -54,25 +43,25 @@ public class ArrayControl
             case "bool": yield target = new Boolean[target.length];
             default: System.out.println("\nARRAY TYPE CONVERSION ERROR\nRETURNING null"); yield null;
         };
-        /*do
-        {
-            if (type.equalsIgnoreCase("short"))
-                return target = new Short[target.length];
-            else if (type.equalsIgnoreCase("int"))
-                return target = new Integer[target.length];
-            else if (type.equalsIgnoreCase("long"))
-                return target = new Long[target.length];
-            else if (type.equalsIgnoreCase("string"))
-                return target = new String[target.length];
-            else if (type.equalsIgnoreCase("char"))
-                return target = new Character[target.length];
-            else if (type.equalsIgnoreCase("bool"))
-                return target = new Boolean[target.length];
-            else
-            {
-                System.out.println("\nARRAY TYPE CONVERSION ERROR\nRETURNING null");
-                return null;
-            }
-        } while(true);*/
-    }
+//        do
+//        {
+//            if (type.equalsIgnoreCase("short"))
+//                return target = new Short[target.length];
+//            else if (type.equalsIgnoreCase("int"))
+//                return target = new Integer[target.length];
+//            else if (type.equalsIgnoreCase("long"))
+//                return target = new Long[target.length];
+//            else if (type.equalsIgnoreCase("string"))
+//                return target = new String[target.length];
+//            else if (type.equalsIgnoreCase("char"))
+//                return target = new Character[target.length];
+//            else if (type.equalsIgnoreCase("bool"))
+//                return target = new Boolean[target.length];
+//            else
+//            {
+//                System.out.println("\nARRAY TYPE CONVERSION ERROR\nRETURNING null");
+//                return null;
+//            }
+//        } while(true);
+    }*/
 }
