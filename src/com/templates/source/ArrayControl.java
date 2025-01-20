e14b0659a66316df616a19792bda2188fa5442ba
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-20 20:44:41 -0300

    First commit

---
+++ src/com/templates/source/ArrayControl.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayControl
+{
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static Object[] manualAddArrSlot(Object[] mainArr)
+    {
+        Object[] tempArr = new Object[mainArr.length + 1];
+        for (int i = 0; i < mainArr.length; i++)
+        {
+            tempArr[i] = mainArr[i];
+        }
+        mainArr = tempArr;
+        return mainArr;
+    }
+
+    //ADDS times AMOUNTS OF SLOTS TO AN ARRAY
+    public static Object[] addArrSlotsWCopy(Object[] mainArr, int times)
+    {
+        return Arrays.copyOf(mainArr, (mainArr.length + times));
+    }
+}
