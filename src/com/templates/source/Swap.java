0af5cef96d2f5895fd10b2b76729f3c876fb982e
BunnyHeart994 <148624558+BunnyHeart994@users.noreply.github.com>
2025-01-21 15:12:04 -0300

    Swap updated

--- src/com/templates/source/Swap.java
+++ src/com/templates/source/Swap.java
@@ -1,14 +1,22 @@
 package com.templates.source;
 
 public class Swap {
-    public static void main(String[] args) {
+    public static Object a, b;
 
-    }
+    //MAIN METHOD FOR TESTS
+    /*public static void main(String[] args) {
+        swap("a", "b");
+        Print.print(Swap.a + " " + Swap.b);
+    }*/
 
-    public static void swapInt(int a, int b) {
-
-    }
-    public static void swapString(String a, String b) {
-
+    public static void swap(Object a, Object b) {
+        Swap.a = b;
+        Swap.b = a;
     }
 }
+//HOW IT WORKS:
+//To swap two values with this class, first call the static method "swap",
+//with 2 parameters - the first is the value that you want to be swapped
+//with the value in the second parameter. Then, to use the values,
+//use the static variables "a" and "b" [the values will already be swapped
+//("a" as "b" and vice versa)].
